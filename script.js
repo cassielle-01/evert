2aa0833681c2bee315af3c362b4afa0fd086a796
cassie <cassiellegerwine@gmail.com>
2025-10-07 12:01:34 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Pilih tombol beradasarkan ID-nya
+const tombolSapa = document.getElementByle('SapaButton)
+// 2. Tambahkan 'event listener' untuk aksi 'click'
+tombolSapa.addEvent:istener('click',function90() {
+  // 3. Jalankan kode ini ketika tombol di-klik
+  alert('Hi! Thank you so much for reaching me out!');
+});
