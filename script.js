40f988d890ff7c57c09b8090f73f01a7835028a2
cassie <cassiellegerwine@gmail.com>
2025-10-07 13:32:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // 1. Pilih tombol berdasarkan ID-nya
 const tombolSapa = document.getElementById('sapaButton');
-// 2. Tambahkan 'event listener' untuk aksi 'click'
+
+// 2. Tombohkan 'event listener' untuk aksi 'click'
 tombolSapa.addEventListener('click', function() {
-  // 3. Jalankan kode ini ketika tombol di-klik
-  alert('Hi! Thank you so much for reaching me out!');
+    // 3. Jalankan kode ini ketika tombol di-klik
+    alert('Terima kasih masih sayang aku!');
 });
